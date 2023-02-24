db9d9ddd47f3bb4cfb8d1159e0b51ec327f777f3
Jui Shah <Jui.Shah@einfochips.com>
2023-02-24 11:13:09 +0530

    add new form exercise

--- demo1_selenium.py
+++ demo1_selenium.py
@@ -2,6 +2,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 driver.get("https://www.facebook.com/")
@@ -22,7 +23,19 @@ driver.find_element(By.NAME,"lastname").send_keys("marry")
 driver.find_element(By.ID,"password_step_input").send_keys("ABCdef123!")
 driver.find_element(By.NAME,"reg_email__").send_keys("abc@gmail.com")
 driver.find_element(By.XPATH,"//input[@value='1']").click()
+select_day = Select(driver.find_element(By.ID,"day"))
+select_day.select_by_value("12")
+select_month = Select(driver.find_element(By.ID,"month"))
+select_month.select_by_value("9")
+select_year = Select(driver.find_element(By.ID,"year"))
+select_year.select_by_value("2015")
 driver.find_element(By.NAME,"websubmit").click()
+
+
+
+
+
+ 
 time.sleep(10)
 #print the title of the page.
 
---
+++ demo3_salesforce.py
@@ -0,0 +1,32 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.get("https://www.salesforce.com/in/form/signup/freetrial-sales/")
+driver.title
+driver.implicitly_wait(30)
+
+
+driver.find_element(By.NAME, "UserFirstName").send_keys( "kin")
+driver.find_element(By.NAME, "UserLastName").send_keys("mike")
+driver.find_element(By.NAME, "UserEmail").send_keys("kin@gmail.com")
+driver.find_element(By.NAME, "UserTitle").click()
+#driver.find_element(By.XPATH, "//select[@name='UserTitle']/option[text()='IT Manager']").click()
+select_Jobtitle = Select(driver.find_element(By.NAME, "UserTitle"))
+select_Jobtitle.select_by_value("Sales_Manager_AP")
+# "//select[@name='element_name']/option[text()='option_text']").click()
+driver.find_element(By.XPATH, "//select[@name='CompanyEmployees']/option[text()='101 - 500 employees']").click()
+select_country=Select(driver.find_element(By.NAME,"CompanyCountry"))
+select_country.select_by_value("US")
+select_state=Select(driver.find_element(By.NAME,"CompanyState"))
+select_state.select_by_value("AL")
+driver.find_element(By.XPATH, "//div[@class='checkbox-ui']").click()
+driver.find_element(By.NAME, "UserPhone").send_keys("9825040367")
+driver.find_element(By.NAME,"CompanyName").send_keys("Arrow")
+driver.find_element(By.NAME,"start my free trial").click()
+#driver.find_element(By.XPATH, "//select[@name='CompanyCountry']/option[text()='United Kingdom']").click()    driver.implicitly_wait(10)    driver.find_element(By.XPATH, "//div[@class='checkbox-ui']").click()    driver.implicitly_wait(10)    driver.find_element(By.NAME, "start my free trial").click()
+time.sleep(25)
+#driver.quit()
